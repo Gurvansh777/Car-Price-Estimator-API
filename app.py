cbc46970ae558ec04aee2a898ca216caa1b59625
EC2 Default User <ec2-user@ip-172-31-15-186.ec2.internal>
2021-03-25 07:56:59 +0000

    API-BETA

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+"""
+sudo pip3 install pandas
+sudo pip3 install scikit-learn
+"""
+from flask import Flask, jsonify, request
+import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/api/price/<string:car_make>/<string:car_name>/<int:year>/<int:odometer>', methods=["GET"])
-def get_price(car_make, car_name, year, odometer):
-    return '<h1>Welcome to Flask! {0} {1} {2} {3} </h1>'.format(car_make, car_name, year, odometer)
+model = pickle.load(open('model/LinearRegressionModel.pkl', 'rb'))
+car = pd.read_csv('data/dataset.csv')
+
+@app.route('/api/price', methods=["GET"])
+def get_price():
+    try:
+        car_make = request.args.get("make")
+        car_name = request.args.get("name")
+        year = request.args.get("year")
+        odometer = request.args.get("odometer")
+        
+        prediction = model.predict(pd.DataFrame(columns=['model', 'manufacturer', 'year', 'odometer'],
+                 data=np.array([car_name, car_make, year, odometer]).reshape(1, 4)))
+        price = prediction[0]
+        if(price < 0):
+            raise Exception
+    except:
+        return jsonify(resultvalid = 0, message = 'car not found!', price = 0)
+    
+    return jsonify(resultvalid = 1, message = 'car found!', price = round(price, 2))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
